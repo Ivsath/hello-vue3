08f91ae123659e2cd948ed5ecc06ea457197a119
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-19 19:24:13 -0500

    Fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 }
 </script>
 
