586e32b53baf0c048956d6b051c43a3d156fcf58
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-27 21:22:24 -0500

    Create counter view and replace it with about

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Counter from '../views/Counter.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
+  { path: '/counter', name: 'Counter', component: Counter },
 ]
 
 const router = createRouter({
---
+++ src/views/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h3>Counter Page</h3>
+    <p>{{ count }}</p>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count
+    },
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.commit('decrement')
+    },
+  },
+}
+</script>
