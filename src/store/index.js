6e424ae5f60723d27ca284b8ff80ab4d1b94db4b
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-27 21:20:34 -0500

    Create vuex store

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    decrement(state) {
+      state.count--
+    },
+  },
+})
+
+export default store
